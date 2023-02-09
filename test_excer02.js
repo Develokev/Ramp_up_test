92ac98e862900bf33e637a88c6eb15f43bb7bd1f
Develokev <develokev@gmail.com>
2023-02-09 13:55:15 +0100

    2nd update

--- test_excer02.js
+++ test_excer02.js
@@ -16,8 +16,11 @@ alert("Tu frase tiene " + (cont_espacios + 1) + " palabras.");
 //Extra: Supón que el usuario puede equivocarse y poner más de un espacio entre palabras,
 //nuestro programa debe seguir contando bien las palabras
 
+// de momento esta idea no ha funcionado (EXTRA)
+
 // for (let i = 0; i < frase.length; i++) {
 //     if (frase[i] == caracter_doble) {
-//         cont_espacios
+//         cont_espacios = (cont_espacios -1)
 //     }
 // }
+// alert("Tu frase tiene " + (cont_espacios + 1) + " palabras.");
